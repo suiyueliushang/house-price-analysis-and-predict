e9626e7cb7282ec194c181aac062dd9c18896d68
runoob <33822377+suiyueliushang@users.noreply.github.com>
2019-08-20 16:16:26 +0800

    django test 1.4

--- hello_world/hello_world/settings.py
+++ hello_world/hello_world/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b$o0-a-opv+t@j*fhy87v8v&t5#jty9wunzakg7sv%(p(habz6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
