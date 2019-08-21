d7c28da49d3f3e0187ead672d51ac0f52fbb03fa
runoob <33822377+suiyueliushang@users.noreply.github.com>
2019-08-21 11:08:03 +0800

    1.3

--- web_server/manage.py
+++ web_server/manage.py
@@ -5,6 +5,19 @@ import sys
 
 
 def main():
+    """主函数，实现接受httprequest并返回响应
+    -----------
+    author:suiyueliushang
+    -----------
+    date:2019.8.21
+    -----------
+    示例
+    -------
+    >>> a=[1,2,3]
+    >>> print [x + 3 for x in a]
+    """
+
+    #设置os默认路径
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_server.settings')
     try:
         from django.core.management import execute_from_command_line
