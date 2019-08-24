fc4c45a594d68a39f0bc11b3675e5212662dfbe2
runoob <33822377+suiyueliushang@users.noreply.github.com>
2019-08-24 14:31:25 +0800

    111

--- web_server/dbconnect.py
+++
@@ -1,9 +0,0 @@
-import PyMySQL
-conn = PyMySQL.connect(
-    host = "localhost",
-    user="root",
-    password="lx15651697362",
-    database="students",
-    charset="utf8",
-)
-cursor = conn.cursor()
