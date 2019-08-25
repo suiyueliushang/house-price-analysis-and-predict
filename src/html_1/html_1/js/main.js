f1148b1a29b527de05901130cfd4bb69db0a1c73
runoob <33822377+suiyueliushang@users.noreply.github.com>
2019-08-25 19:50:13 +0800

    1.1

---
+++ src/html_1/html_1/js/main.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function(){
+
+	//Code to center the subscription pup-up box
+	$box = $('#box');
+	$box.css({
+		'left' : '50%',
+		'top' : '50%',
+		'margin-left' : -$box.width()/2 + 'px',
+		'margin-top' : -$box.height()/2 + 'px'
+	});
+
+
+	//Subscription pup-up
+	$('.start_btn').click(function(){
+		$('#lightbox').width($(window).width()).height($(window).height()).fadeIn(200);
+		$('#box').fadeIn(200);
+
+		return false;
+	});
+
+	$('#lightbox, .close').click(function(){
+		$('#lightbox').width(0).height(0).fadeOut(200);
+		$('#box').fadeOut(200);
+
+		return false;
+	});
+
+
+});
