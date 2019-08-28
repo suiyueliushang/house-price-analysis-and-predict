ee72cdbff287006b9c126b7f4d5aff6f689e1782
XJF0609 <805823906@qq.com>
2019-08-28 19:58:35 +0800

    后端代码2.0

---
+++ pipi/house/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('sign_in_by_password',views.sign_in_by_password),
+    path('sign_in_by_phone_number',views.sign_in_by_phone_number),
+    
+    
+    #path('input/',views.index,name="index"),
+    #path('',views.login)
+]
