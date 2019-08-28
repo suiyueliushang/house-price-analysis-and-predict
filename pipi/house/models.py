d3ea51a81fc30c20bb8f78fea2ab7bf20383feec
Tessic <jiangyaqichn@gmail.com>
2019-08-28 20:48:26 +0800

    update tables.html

---
+++ pipi/house/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class User(models.Model):
+
+    user_name=models.CharField(max_length=20,null=False)
+    password=models.CharField(max_length=100,null=False)
+    user_phone=models.CharField(max_length=20,null=False)
+    user_mail=models.CharField(max_length=30,null=False,default=0)
+    session_id=models.CharField(max_length=50,null=False,default=0)
+
+    class Meta:
+        db_table="user_info"
--- src/web_server/web_server/sign_in_up/sign_in_up_view.py
+++ src/web_server/web_server/sign_in_up/sign_in_up_view.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from .. import dbconnect
 
 
-@csrf_exempt
+
 def sign_in_by_password(request):
     '''登录传输的用户名和密码
     @arg：str:request.post[user_name]
@@ -33,16 +33,24 @@ def is_resgistered_username(request):
 def sign_in_by_phone_number(request):
     '''手机号登录
     传递一个正确的11位中国大陆手机号码，和用户出入的验证码
-        @args：phonenumber，auth_code
+        @args：phone_number，auth_code
         @return：sessionid，为空，则表示用户输入验证码错误
-    '''
+        '''    
+    print("用户名："+str(request.POST.get('phone_number')))
+    print('密码：'+str(request.POST.get('password')))
+    return HttpResponse('hello,world')
+
 
 def sign_up(request):
     '''注册
     传递用户名，手机号，还有密码,验证码
-    @args：user_name,phone_number,password,auth_code
+    @args：reg_user_name,reg_phone_number,reg_password,reg_auth_code
     @return bool，判断用户验证码是否正确
     '''
+    print("用户名："+str(request.POST.get('reg_user_name')))
+    print("手机号："+str(request.POST.get('reg_phone_number')))
+    print('密码：'+str(request.POST.get('reg_password')))
+    return HttpResponse('hello,world')
 
 def forget_password(request):
     '''忘记密码
