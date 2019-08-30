94573a38fdd5d624544a47fe808df6b185752972
XJF0609 <805823906@qq.com>
2019-08-30 11:17:12 +0800

    8-30

--- pipi/house/urls.py
+++ pipi/house/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
     path('sign_in_by_phone_number',views.sign_in_by_phone_number),
     path('reg_phone_number',views.reg_phone_number),
     path('sign_up',views.sign_up),
-    
+    path('forget_password',views.forget_password),
+    path('get_auth_code',views.get_auth_code),
+    path('query_prices',views.query_prices)
+
     #path('input/',views.index,name="index"),
     #path('',views.login)
 ]
