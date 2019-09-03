cbc6e5933ff574a6dbe2f0d26d973627fe4a1e83
Dreamy <663637128@qq.com>
2019-09-03 10:13:38 +0800

    search_member

--- src/PPHouseV1.0/js/front.js
+++ src/PPHouseV1.0/js/front.js
@@ -655,12 +655,13 @@ $("#forget_password").click(function() {
 });
 
 
+//管理员查找图片
 $("#admin_search").click(function() {
 
     search_phone = $('#search_phone').val();
     $.ajax({
         type: "POST", //提交的方法
-        url: "/get_auth_code", //提交的地址  
+        url: "/search_member", //提交的地址  
         // contentType: false,
         data: {
             'search_phone': search_phone,
@@ -670,21 +671,17 @@ $("#admin_search").click(function() {
         //$('#login_form').serialize(), // 序列化表单值  
         async: false,
         error: function(request) { //失败的话
-
             alert("Connection error");
-            document.getElementById("search_div").style.display = "";
-            document.getElementById("search_time").innerText = "1";
-            document.getElementById("search_user").innerText = "2";
-            document.getElementById("search_phone_number").innerText = "3456789+";
+            document.getElementById("error_info").style.display = "";
 
         },
         success: function(data) { //成功
 
-            document.getElementById("search_div").style.display = "";
-            document.getElementById("search_time").innerText = "1";
-            document.getElementById("search_user").innerText = "2";
-            document.getElementById("search_phone_number").innerText = "34567";
-
+            document.getElementById("error_info").style.display = "none";
+            document.getElementById("search_div").style.display = "block";
+            //document.getElementById("search_time").innerText = data.
+            document.getElementById("search_user").innerText = data.user.user_name;
+            document.getElementById("search_phone_number").innerText = data.user.user_phone;
 
         }
 
