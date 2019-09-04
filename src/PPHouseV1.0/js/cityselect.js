7da039847a49ff2edc44256494d804cd9c3fd576
Tessic <jiangyaqichn@gmail.com>
2019-09-04 17:13:51 +0800

    update tables.html

--- src/PPHouseV1.0/js/cityselect.js
+++ src/PPHouseV1.0/js/cityselect.js
@@ -343,42 +343,145 @@ Vcity.CitySelector.prototype = {
         var houses;
         var links = Vcity._m.$('a',this.hotCity);
         var that = this;
+        var current_city;
+
+        
+        function city_index(city){
+            if(city.name == current_city){
+                return city;
+            }
+        };
+
         for(var i=0,n=links.length;i<n;i++){
             links[i].onclick = function(){
                 that.button.innerHTML = this.innerHTML;
-                $.ajax({
-                    type: "post",
-                    url: "/query_prices",
-                    data: {
-                        "city": this.innerHTML,
-                        
-                        "time": date.getDate,
-                        "min": 0,
-                        "max": 100000000
-                    },
-                    datatype:"json",
-                    async: false,
-                    error: function(request) { //失败的话
-                        alert("Connection error");
-                    },
-                    success(data){
-                        switch(data.is_success){
-                            case '0':{
-                                houses = data.houses;
-                                break;
-                            }
-                            case '1':{
-                                alert("数据传送错误");
-                                break;
-                            }
-                            default:
-                                alert("未知错误");
-                        }
+                current_city = this.innerHTML;
+                
+                $(".load").remove();
+                if (current_city != null) {
+                    var districts = citylist[citylist.findIndex(city_index)].district.length;
+                    for (var n = 0; n < districts; n++) {
+                        $(".select1 .select-all").after('<dd class="load"><a href="#">'+citylist[citylist.findIndex(city_index)].district[n]+'</a></dd>');
                     }
-                })
+                }
+
                 Vcity._m.addClass('hide',that.cityBox);
                 /* 点击城市名的时候隐藏myIframe */
                 Vcity._m.addClass('hide',that.myIframe);
+
+                $(document).ready(function(){
+                    $("#select1 dd").click(function () {
+                        $(this).addClass("selected").siblings().removeClass("selected");
+                        if ($(this).hasClass("select-all")) {
+                            $("#selectA").remove();
+                        } else {
+                            var copyThisA = $(this).clone();
+                            if ($("#selectA").length > 0) {
+                                $("#selectA a").html($(this).text());
+                            } else {
+                                $(".select-result dl").append(copyThisA.attr("id", "selectA"));
+                            }
+                        }
+                    });
+                });
+
+                var region_price = new Array(12);
+                $.ajax({
+                    type: "post",
+                    url: "/query_prices",
+                    datatype:"json",
+                    data: {
+                        "city": current_city,
+                        "month": getMonth()+1,
+                        "min": 0,
+                        "max": 10000000,
+                        'page':1
+                    },
+                    async: false,
+                    error: function(request) {
+                        alert("Connection error");
+                    },
+                    success(data){
+                        region_price[0]=data.one;
+                        region_price[1]=data.two;
+                        region_price[2]=data.three;
+                        region_price[3]=data.four;
+                        region_price[4]=data.five;
+                        region_price[5]=data.six;
+                        region_price[6]=data.seven;
+                        region_price[7]=data.eight;
+                        region_price[8]=data.nine;
+                        region_price[9]=data.ten;
+                        region_price[10]=data.eleven;
+                        region_price[11]=data.twelve;
+                        $(document).ready(function(){
+                            for(var i=0; i<20; i++){
+                                $(".house_title").eq(i).html(data.houses[i].firm_name);
+                                $(".address").eq(i).html(data.houses[i].address);
+                                $(".house_type").eq(i).html(data.houses[i].house_type);
+                                $(".ave_price").eq(i).html(data.houses[i].average_price);
+                                $(".total_price").eq(i).html(data.houses[i].total_price);
+                                $(".area").eq(i).html(data.houses[i].area);
+                            }
+                            $("#Pagination").pagination(data.page_num);
+                            $("#allPage").html(data.page_num);
+                        });
+                        }
+                });
+                $("#lineChartExample4").remove();
+                $("#line_chart").append('<canvas id="lineChartExample4"></canvas>');
+                $(document).ready(function () {
+                    'use strict';
+                var LINECHART4 = $('#lineChartExample4');
+                var myLineChart4 = new Chart(LINECHART4, {
+                    type: 'line',
+                    options: {
+                        scales: {
+                            xAxes: [{
+                                display: true,
+                                gridLines: {
+                                    display: false
+                                }
+                            }],
+                            yAxes: [{
+                                display:true,
+                                gridLines: {
+                                    display: true
+                                }
+                            }]
+                        },
+                        legend: {labels:{fontColor:"#777", fontSize: 12},display:false}
+                    },
+                    data: {
+                        labels: [lastYear(getMonth()-11), lastYear(getMonth()-10), lastYear(getMonth()-9), lastYear(getMonth()-8), lastYear(getMonth()-7), lastYear(getMonth()-6), lastYear(getMonth()-5), lastYear(getMonth()-4), lastYear(getMonth()-3), lastYear(getMonth()-2), lastYear(getMonth()-1), lastYear(getMonth())],
+                        datasets: [
+                            {
+                                label: "City 1",
+                                fill: true,
+                                lineTension: 0,
+                                backgroundColor: "transparent",
+                                borderColor: '#6ccef0',
+                                pointBorderColor: '#59c2e6',
+                                pointHoverBackgroundColor: '#59c2e6',
+                                borderCapStyle: 'butt',
+                                borderDash: [],
+                                borderDashOffset: 0.0,
+                                borderJoinStyle: 'miter',
+                                borderWidth: 3,
+                                pointBackgroundColor: "#59c2e6",
+                                pointBorderWidth: 0,
+                                pointHoverRadius: 4,
+                                pointHoverBorderColor: "#fff",
+                                pointHoverBorderWidth: 0,
+                                pointRadius: 4,
+                                pointHitRadius: 0,
+                                data: [region_price[0], region_price[1], region_price[2], region_price[3], region_price[4], region_price[5], region_price[6], region_price[7], region_price[8], region_price[9], region_price[10], region_price[11]],
+                                spanGaps: false
+                            }
+                        ]
+                    }
+                });
+                });
             }
         }
     },
