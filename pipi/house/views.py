7364468312de344f6fffa82e7f8a878c019253a4
XJF0609 <805823906@qq.com>
2019-09-04 11:13:21 +0800

    Update views.py

--- pipi/house/views.py
+++ pipi/house/views.py
@@ -450,17 +450,38 @@ def new_sign_in_list(request):
     third={'hour':third_time[0],'minute':third_time[1],'user_name':users[0].user_name,'user_phone':users[0].user_phone}
     result={'first':first,'second':second,'third':third}
     return HttpResponse(json.dumps(result,ensure_ascii=False),content_type="application/json,charset=utf-8")
+
 def new_sign_up(request):
     '''
     return 新增用户趋势
     '''
-    
+    new_sign=list(New_user_number.objects.all())
+    print(new_sign[0])
+    new=[]
+    for i in range(0,12):
+        if(new_sign[-1-i]):
+            new[11-i]=new_sign[-1-i].number
+        else:
+            new[11-i]=0
+    result={'new':new}
+    return HttpResponse(json.dumps(result,ensure_ascii=False),content_type="application/json,charset=utf-8")
 
 
 def new_sign_in(request):
     '''
     return 访问用户趋势
     '''
+    new_sign=list(Visitor_number.objects.all())
+    print(new_sign[0])
+    new=[]
+    for i in range(0,12):
+        if(new_sign[-1-i]):
+            new[11-i]=new_sign[-1-i].number
+        else:
+            new[11-i]=0
+    result={'new':new}
+    return HttpResponse(json.dumps(result,ensure_ascii=False),content_type="application/json,charset=utf-8")
+
 
 def search_member(request):
     '''
