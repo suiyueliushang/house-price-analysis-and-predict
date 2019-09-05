5a26d0098be6b1c6f03477700ca11b0cb806905c
Dreamy <663637128@qq.com>
2019-09-05 19:36:57 +0800

    update

--- src/PPHouseV1.0/js/front.js
+++ src/PPHouseV1.0/js/front.js
@@ -1062,3 +1062,84 @@ function get_admin_session() {
 
     });
 }
+
+//管理员增加新房源
+$("#admin_add").click(function() {
+
+    date = $('#date').val();
+    province = $('#province').val();
+    city = $('#city').val();
+    district = $('#district').val();
+
+
+    address = $('#address').val();
+    firm_name = $('#firm_name').val();
+    house_type = $('#house_type').val();
+    direction = $('#direction').val();
+
+    area = $('#area').val();
+    average_price = $('#average_price').val();
+    total_price = $('#total_price').val();
+    elevator = $('#elevator').val();
+
+    heigth = $('#heigth').val();
+    huxing_jiegou = $('#huxing_jiegou').val();
+    jianzhuleixing = $('#jianzhuleixing').val();
+    new_h = $('#new_h').val();
+
+    nianxian = $('#nianxian').val();
+    ti_bili = $('#ti_bili').val();
+    zhuangxiu = $('#zhuangxiu').val();
+    kaiaipan_shijian = $('#kaiaipan_shijian').val();
+
+    $.ajax({
+        type: "POST", //提交的方法
+        url: "/add_users", //提交的地址  
+        // contentType: false,
+        data: {
+            'date': date,
+            'province': province,
+            'city': city,
+            'district': district,
+
+            'address': address,
+            'firm_name': firm_name,
+            'house_type': house_type,
+            'direction': direction,
+
+            'area': area,
+            'average_price': average_price,
+            'total_price': total_price,
+            'elevator': elevator,
+
+            'heigth': heigth,
+            'huxing_jiegou': huxing_jiegou,
+            'jianzhuleixing': jianzhuleixing,
+            'new_h': new_h,
+
+            'nianxian': nianxian,
+            'ti_bili': ti_bili,
+            'zhuangxiu': zhuangxiu,
+            'kaiaipan_shijian': kaiaipan_shijian,
+
+        },
+
+        datatype: "json",
+        //$('#login_form').serialize(), // 序列化表单值  
+        async: false,
+        error: function(request) { //失败的话
+            alert("Connection error");
+        },
+        success: function(data) { //成功
+            switch (data.is_success) {
+                case '0':
+                    alert("提交成功");
+                    break;
+                default:
+                    alert("未知错误");
+
+            }
+        }
+
+    });
+});
